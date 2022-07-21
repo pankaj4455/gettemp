f4f2500b440bdec41fc2c67dcca633f8f19553b4
pankaj yadav <pankajyadav3436@gmail.com>
2022-07-21 16:05:29 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
 
 function App() {
+  const[data,setData]=useState()
+  const[datas,setDatas]=useState();
+  const change=(e)=>{
+    setData(e.target.value);
+  }
+ 
+ useEffect(()=>{
+  const fetchdata=async ()=>{
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${data}&units=metric&appid=733f43c1bbd894ee884dde9e92eae0cc`
+    var res=await fetch(url);
+    const out= await res.json();
+  
+    setDatas(out.main.temp);
+
+    
+    }
+    fetchdata()
+  })
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  <h3 id="h3">  Single Page Application & Live Search</h3>
+    <div className="out">
+      
+    <input type="text" value={data} onChange={change} placeholder="Search location name"/>
+    
+    <h2>{data} {datas}°Cel</h2>
+      
     </div>
+  
+  
+    </>
   );
 }
 
